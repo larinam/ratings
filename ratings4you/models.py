8cb6c70229fcad7ddc88e765b4f5d72eeac9fda8
Hatter <krater@narod.ru>
2010-06-18 00:34:13 +0600

    models description started

--- ratings4you/models.py
+++ ratings4you/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+# 
+class RatingThemesDirectory(models.Model):
+    """
+    Справочник тем для голосований.
+    """
+    pass
+
+class RegionDirectory(models.Model):
+    """
+    Справочник регионов - здесь могут быть как страны, города, области, так и всё что угодно.
+    """
+    pass
+
+class  Rating(models.Model):
+    """
+    Сам рейтинг, содержащий пункты для голосования.
+    """
+    pass
+
+class RatingItem(models.Model):
+    """
+    Пункт рейтинга в @see: Rating .
+    """
+    pass
+
+class Vote(models.Model):
+    """
+    Голос, отданный за тот или иной пункт рейтинга.
+    """
+    pass
