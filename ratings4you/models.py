d0228ebf0171a60c70d6a2fde01c188c939c5b8a
Hatter <krater@narod.ru>
2010-06-19 19:52:03 +0600

    base methods added to model

--- ratings4you/models.py
+++ ratings4you/models.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
+from datetime import datetime
+from interfaces import IModeratable
 
 # Create your models here.
 
 # 
+class UserProfile(models.Model):
+    juridical_name = models.CharField(max_length=255, help_text="Название юридического лица") #Название организации
+    contact_person = models.CharField(max_length=255, help_text="Контактное лицо") #Контактное лицо
+    inn = models.CharField(max_length=255, help_text="ИНН")
+    address = models.TextField(help_text="Адрес")
+    phone_numer = models.CharField(max_length=255, help_text="Номер телефона")
+    user = models.ForeignKey(User, unique=True)
+    
 class RatingThemesDirectory(models.Model):
     """
     Справочник тем для голосований.
@@ -16,27 +26,57 @@ class RegionDirectory(models.Model):
     """
     name = models.CharField()
 
-class  Rating(models.Model):
+class  Rating(models.Model, IModeratable):
     """
     Сам рейтинг, содержащий пункты для голосования.
+    Один из главных бизнес-объектов.
     """
     name = models.CharField()
     region = models.ForeignKey(RegionDirectory, unique=True)
     theme = models.ForeignKey(RatingThemesDirectory, unique=True)
     begin_date = models.DateField()
     end_date = models.DateField()
+    moderated = models.BooleanField(default=False)
     
+    def addRatingItem(self, name):
+        rating_item = RatingItem(name=name, rating=self)
+        rating_item.save()
+        return rating_item
+        
+    def listRatingItems(self):
+        return RatingItem.objects.filter(rating=self)
     
-class RatingItem(models.Model):
+    def isModerated(self):
+        return moderated
+    
+    def setModerated(self, value=True):
+        self.moderated = value
+
+    
+class RatingItem(models.Model, IModeratable):
     """
     Пункт рейтинга в @see: Rating .
+    Один из главных бизнес-объектов.
     """
     name = models.CharField()
     rating = models.ForeignKey(Rating, unique=True)
+    moderated = models.BooleanField(default=False)
+    
+    def addVote(self, user, ip):
+        vote = Vote(user=user, rating_item=self, ip=ip)
+        vote.save()
+        
+    def isModerated(self):
+        return moderated
+        
+    def setModerated(self, value=True):
+        self.moderated = value
 
 class Vote(models.Model):
     """
     Голос, отданный за тот или иной пункт рейтинга. @see: RatingItem
+    Один из главных бизнес-объектов.
+    Не должно быть возможности как-то изменить.
     """
     user = models.ForeignKey(User, unique=True)
     vote_date = models.DateTimeField(auto_now=True)
---
+++ ratings4you/urls.py
@@ -0,0 +1,10 @@
+'''
+Created on 19.06.2010
+
+@author: alarin
+'''
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+                       
+)
