3a10c2d5efa059110f64c84796c6b6181ad5d18f
Hatter <krater@narod.ru>
2010-07-04 14:45:55 +0600

    user name tag converts user.id to user.username

---
+++ ratings4you/templatetags/user_name_tag.py
@@ -0,0 +1,34 @@
+'''
+@since: 20.12.2009
+@author: alarin
+'''
+
+from django import template
+from django.template import Context
+from django.contrib.auth.models import User
+
+register = template.Library()
+
+class UserNameTagNode(template.Node):
+    
+    def __init__(self, user_id=None):
+        self.user_id = template.Variable(user_id)
+
+    def render(self, context):
+        #raise template.TemplateSyntaxError, context
+        user_id = self.user_id.resolve(context)
+        request = context['request']
+        author = User.objects.get(pk=int(user_id))
+        return author.username
+
+
+@register.tag(name="user_id_to_name")
+def do_user_id_to_name_tag(parser, token):
+    #raise template.TemplateSyntaxError, parser
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name = token.split_contents()
+        user_id = token.split_contents()[1]
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r requires 1 argument" % token.contents.split()[0]
+    return UserNameTagNode(user_id=user_id)
