3fbeb19e3e0c69674e0b2ca67d08b8232cc9677a
Hatter <krater@narod.ru>
2010-07-08 00:14:32 +0600

    permissions for admin. jquiry for comments show hide added

--- ratings4you/views_admin.py
+++ ratings4you/views_admin.py
@@ -1,22 +1,26 @@
 # -*- coding: utf8 -*-
 # Create your views here.
+from db_rating import *
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from forms import RatingModelForm, RatingThemesDirectoryForm, \
     RegionDirectoryForm, RatingItemForm
 from models import RegionDirectory, RatingThemesDirectory, MODERATABLE_MODELS
-from db_rating import *
 from ratings4you.models import RatingItem
 
 
+@login_required
 def index(request):
     return render_to_response('ratings/index_admin.html',
                               context_instance=RequestContext(request))
-    
+
+@login_required
 def catalogs(request):
     return render_to_response('ratings/admin/catalogs_admin.html',
                               context_instance=RequestContext(request))
-    
+
+@login_required    
 def themes(request):
     catalog_items = RatingThemesDirectory.objects.all().order_by('name')
     if request.POST:
@@ -35,6 +39,7 @@ def themes(request):
                                    link_text="или вернитесь в список каталогов", catalog_items=catalog_items),
                               context_instance=RequestContext(request))
 
+@login_required
 def regions(request):
     catalog_items = RegionDirectory.objects.all().order_by('name')
     if request.POST:
@@ -59,6 +64,7 @@ def regions(request):
                                    catalog_items=catalog_items),
                               context_instance=RequestContext(request))
 
+@login_required
 def moderation(request):
     moderatable = Rating.objects.filter() #listActualRatings()
     return render_to_response('ratings/admin/moderation_admin.html', 
@@ -68,7 +74,8 @@ def moderation(request):
                                    link_text="или вернитесь в раздел администрирование",
                                    ),
                               context_instance=RequestContext(request))
-    
+
+@login_required
 def moderate_rating(request, id):
     rating = Rating.objects.get(pk=id)
     moderatable = rating.listRatingItems()
