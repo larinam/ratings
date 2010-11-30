155e5b4e9fa6f5308e1a1aa22c1e7ed618957ec2
alarin <alarin@alarin.(none)>
2010-12-01 00:21:18 +0500

    2 fixes

--- ratings4you/models.py
+++ ratings4you/models.py
@@ -214,7 +214,7 @@ class RatingItem(models.Model, IModeratable, NameAsIdentifier):
         return Vote.objects.filter(rating_item=self).count()
     
     def getPercentage(self):
-        return self.getOverallCount()/(self.rating.votesCount() or 1)*100
+        return float(self.getOverallCount())/(self.rating.votesCount() or 1.0)*100
     
     def setName(self, value):
         self.name = value
